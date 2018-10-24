1ae5db1bdf3072c50a1972fa2caa52c3f2940394
Marist User <techengineervivek@gmail.com>
2018-10-24 15:28:10 -0400

    configured sqllite using SQLite JDBC Driver

---
+++ version1/maven_use/eclipse_processmonitor_ver1/processmonitorver1/src/main/java/ConnectionEstablish/ConnectToDB.java
@@ -0,0 +1,55 @@
+/**
+ * This package will be the single point of contact for any kind of connection requests to DB
+ */
+package ConnectionEstablish;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+/**
+ *
+ * @author Vivek
+ */
+public class ConnectToDB {
+	
+	private static Connection cxn = null;
+	
+     /**
+     * This class helps to connect with SQLite Database
+     * 
+     */
+    private static Connection connectToSQLiteDB() {
+        try {
+            // setting up the path of the SQLite database
+            String db_filepath = "jdbc:sqlite:taskprocessmgr.db";
+            
+            // create a connection to the database
+            cxn = DriverManager.getConnection(db_filepath);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        // make sure reception of this cxn requester is doing null check
+        return cxn;
+    }
+    
+    /**
+     * This will the only point of connect from outside world to get connection instance of configured SQLite
+     *      
+     * @return Connection instance
+     */
+    public static Connection getSQLiteConnection() {
+    	return connectToSQLiteDB();
+    }
+    
+    /**
+     * @param args command line args
+     */
+    public static void main(String[] args) {
+    	System.out.print("cxn: " + getSQLiteConnection());
+    }
+}
