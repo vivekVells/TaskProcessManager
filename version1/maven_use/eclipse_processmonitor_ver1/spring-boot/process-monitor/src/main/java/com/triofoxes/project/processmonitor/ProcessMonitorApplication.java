b67dfbb3b8c6a0a43afbb54bf98a47d0806ef047
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-10-24 23:56:50 -0400

    bkups and other required initial setup of version 1 of project done

--- version1/maven_use/eclipse_processmonitor_ver1/spring-boot/process-monitor/src/main/java/com/triofoxes/project/processmonitor/ProcessMonitorApplication.java
+++ version1/maven_use/eclipse_processmonitor_ver1/spring-boot/process-monitor/src/main/java/com/triofoxes/project/processmonitor/ProcessMonitorApplication.java
@@ -2,8 +2,16 @@ package com.triofoxes.project.processmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author Vivek
+ * 
+ * Main Starting point for the Process Monitor Application
+ *
+ */
 @SpringBootApplication
+@ComponentScan
 public class ProcessMonitorApplication {
 
 	public static void main(String[] args) {
--- version1/maven_use/eclipse_processmonitor_ver1/spring-boot/process-monitor/src/test/java/com/triofoxes/project/processmonitor/ProcessMonitorApplicationTests.java
+++ version1/maven_use/eclipse_processmonitor_ver1/spring-boot/process-monitor/src/test/java/com/triofoxes/project/processmonitor/ProcessMonitorApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * @author Vivek
+ * 
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProcessMonitorApplicationTests {
