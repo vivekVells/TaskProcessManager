168f37c54c5d7d4ea927f94d1f22364ee4e858c3
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-10-24 22:42:03 -0400

    initial maven - spring boot env setup using spring boot initializer. default dependecies given are web, actuator and devtools

---
+++ version1/maven_use/eclipse_processmonitor_ver1/spring-boot/process-monitor/src/main/java/com/triofoxes/project/processmonitor/ProcessMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.triofoxes.project.processmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessMonitorApplication.class, args);
+	}
+}
