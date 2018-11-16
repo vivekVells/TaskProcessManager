bf203865add21400f1a2867ff7f31a2955ed99f6
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-11-16 00:19:38 -0500

    modularized version class code. an utility class file have to be created soon for common pool functions

--- version1/maven_use/eclipse_processmonitor_ver1/processmonitorver1/src/main/java/kev/systeminfo/Version.java
+++ version1/maven_use/eclipse_processmonitor_ver1/processmonitorver1/src/main/java/kev/systeminfo/Version.java
@@ -4,18 +4,15 @@
  */
 package kev.systeminfo;
 
-import java.io.File;
-import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
+import java.util.HashMap;
 import org.hyperic.sigar.OperatingSystem;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.SigarLoader;
 import org.hyperic.sigar.cmd.Shell;
 import org.hyperic.sigar.cmd.SigarCommandBase;
-import org.hyperic.sigar.win32.LocaleInfo;
 
 /**
  * @author Vivek
@@ -25,112 +22,122 @@ import org.hyperic.sigar.win32.LocaleInfo;
 public class Version extends SigarCommandBase {
 
 
-    public Version(Shell shell) {
-        super(shell);
-    }
+    public Version(Shell shell) { super(shell); }
 
-    public Version() {
-        super();
-    }
+    public Version() { super(); }
 
-    public String getUsageShort() {
-        return "Display sigar and system version info";
-    }
-
-    private static String getHostName() {
+	@Override
+	public void output(String[] arg0) throws SigarException { }
+	
+	/**
+	 * Retrieve Host name of the machine
+	 * 
+	 * @return String host name
+	 */
+    public String getHostName() {
         try {
             return InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
             return "unknown";
         }
     }
-
-    private static void printNativeInfo(PrintStream os) {
-        String version =
-            "java=" + Sigar.VERSION_STRING +
-            ", native=" + Sigar.NATIVE_VERSION_STRING;
-        String build =
-            "java=" + Sigar.BUILD_DATE +
-            ", native=" + Sigar.NATIVE_BUILD_DATE;
-        String scm =
-            "java=" + Sigar.SCM_REVISION +
-            ", native=" + Sigar.NATIVE_SCM_REVISION;
-        String archlib =
-            SigarLoader.getNativeLibraryName();
-
-        os.println("Sigar version......." + version);
-        os.println("Build date.........." + build);
-        os.println("SCM rev............." + scm);
-        String host = getHostName();
-        String fqdn;
-        Sigar sigar = new Sigar(); 
-        try {
-            File lib = sigar.getNativeLibrary();
-            if (lib != null) {
-                archlib = lib.getName();
-            }
-            fqdn = sigar.getFQDN();
-        } catch (SigarException e) {
-            fqdn = "unknown";
-        } finally {
-            sigar.close();
-        }
-
-        os.println("Archlib............." + archlib);
-
-        os.println("Current fqdn........" + fqdn);
-        if (!fqdn.equals(host)) {
-            os.println("Hostname............" + host);
-        }        
-
-        if (SigarLoader.IS_WIN32) {
-            LocaleInfo info = new LocaleInfo();
-            os.println("Language............" + info);
-            os.println("Perflib lang id....." +
-                       info.getPerflibLangId());
-        }
+    
+    /**
+     * Retrieve Fully Qualified Domain Name of the machine
+     * 
+     * @return FQDN
+     */
+    public String getFQDN() {    	
+    	try {
+			return getSigarObject().getFQDN();
+		} catch (SigarException e) {
+			return "unkown";
+		}
     }
     
-    public static void printInfo(PrintStream os) {
-        try {
-            printNativeInfo(os);
-        } catch (UnsatisfiedLinkError e) {
-            os.println("*******ERROR******* " + e);
-        }
-
-        os.println("Current user........" +
-                   System.getProperty("user.name"));
-        os.println("");
-        
-        OperatingSystem sys = OperatingSystem.getInstance();
-        os.println("OS description......" + sys.getDescription());
-        os.println("OS name............." + sys.getName());
-        os.println("OS arch............." + sys.getArch());
-        os.println("OS machine.........." + sys.getMachine());
-        os.println("OS version.........." + sys.getVersion());
-        os.println("OS patch level......" + sys.getPatchLevel());
-        os.println("OS vendor..........." + sys.getVendor());
-        os.println("OS vendor version..." + sys.getVendorVersion());
-        if (sys.getVendorCodeName() != null) {
-            os.println("OS code name........" + sys.getVendorCodeName());
-        }
-        os.println("OS data model......." + sys.getDataModel());
-        os.println("OS cpu endian......." + sys.getCpuEndian());
-
-        os.println("Java vm version....." + 
-                   System.getProperty("java.vm.version"));
-        os.println("Java vm vendor......" + 
-                System.getProperty("java.vm.vendor"));
-        os.println("Java home..........." +
-                System.getProperty("java.home"));
+    /**
+     * Get Username of the machine
+     * 
+     * @return username
+     */
+    public String getUsername() {
+    	return System.getProperty("user.name");
+    }
+    
+    /**
+     * Retrieve desired os version related info 
+     * 
+     * @return mapped os version info
+     */
+    public HashMap<String, Object> getOsVersionInfo() {
+    	HashMap<String, Object> osVersionMap = new HashMap<String, Object>();
+    	OperatingSystem osObj = OperatingSystem.getInstance();
+    	
+    	osVersionMap.put("os description", osObj.getDescription());
+    	osVersionMap.put("os name", osObj.getName());
+    	osVersionMap.put("os arch", osObj.getArch());
+    	osVersionMap.put("os machine", osObj.getMachine());
+    	osVersionMap.put("os version", osObj.getVersion());
+    	osVersionMap.put("os patch level", osObj.getPatchLevel());
+    	osVersionMap.put("os vendor version", osObj.getVendorVersion());
+    	osVersionMap.put("os vendor code name", osObj.getVendorCodeName());
+    	osVersionMap.put("os data model", osObj.getDataModel());
+    	osVersionMap.put("os cpu endian", osObj.getCpuEndian());
+    	
+    	return osVersionMap;
+    }
+    
+    /**
+     * Retrieve desired java version related info
+     * 
+     * @return mapped java version info
+     */
+    public HashMap<String, Object> getJavaVersionInfo() {
+    	HashMap<String, Object> javaVersionMap = new HashMap<String, Object>();
+    	
+    	javaVersionMap.put("java vm version", System.getProperty("java.vm.version"));
+    	javaVersionMap.put("java vm vendor", System.getProperty("java.vm.vendor"));
+    	javaVersionMap.put("java vm home", System.getProperty("java.vm.home"));
+    	
+    	return javaVersionMap;
+    }
+    
+    /**
+     * Retrieve all desired sigar version info
+     * 
+     * @return mapped sigar version info
+     */
+    public HashMap<String, Object> getSigarVersionInfo() {
+    	HashMap<String, Object> sigarVersionMap = new HashMap<String, Object>();
+    	sigarVersionMap.put("sigar java version", Sigar.VERSION_STRING);
+    	sigarVersionMap.put("sigar native version", Sigar.NATIVE_VERSION_STRING);
+    	sigarVersionMap.put("sigar build date", Sigar.BUILD_DATE);
+    	sigarVersionMap.put("sigar native build date", Sigar.NATIVE_BUILD_DATE);
+    	sigarVersionMap.put("sigar scm version", Sigar.SCM_REVISION);
+    	sigarVersionMap.put("sigar native scm version", Sigar.NATIVE_SCM_REVISION);
+    	sigarVersionMap.put("sigar archlib", SigarLoader.getNativeLibraryName()); 	
+    	
+    	return sigarVersionMap;
+    }
+    
+    /**
+     * Get Version class object
+     * 
+     * @return creates & returns newly created version object
+     */
+    public Version getVersionObjection() {
+    	return new Version();
+    }
+    
+    /**
+     * Creates & returns new sigar object
+     * 
+     * @return newly created sigar object
+     */
+    public static Sigar getSigarObject() {
+    	return new Sigar();
     }
 
-    public void output(String[] args) {
-        printInfo(this.out);
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Version().processCommand(args);
-    }
-
+    public static void main(String[] args) throws Exception { }
 }
+
