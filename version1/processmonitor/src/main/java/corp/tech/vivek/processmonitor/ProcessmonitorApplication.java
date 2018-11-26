e70913b297e10f6e4279e3a7da499be9abc63dfc
Marist User <vivekVells>
2018-11-26 15:41:33 -0500

    deprecated old one and created new spring boot project with web, jpa, actuator, embedded mongodb, apache derby

---
+++ version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/ProcessmonitorApplication.java
@@ -0,0 +1,12 @@
+package corp.tech.vivek.processmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessmonitorApplication.class, args);
+	}
+}
