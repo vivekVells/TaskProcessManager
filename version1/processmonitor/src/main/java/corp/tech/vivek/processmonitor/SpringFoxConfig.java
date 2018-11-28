61eb158fed2a4282c053341c87288f9c0fa19695
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-11-28 03:40:33 -0500

    added demo files.

--- version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/SpringFoxConfig.java
+++ version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/SpringFoxConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  * @author Vivek Vellaiyappan | vivekvellaiyappans@gmail.com
@@ -14,12 +18,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+    // .apis(RequestHandlerSelectors.any()) when considering any possible end points in the project
+    // .apis(RequestHandlerSelectors.basePackage("corp.tech")) when considering corp.tech related end points alone in the project
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("corp.tech"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    /**
+     * ApiInfo object, which provides general information about the API
+     *  such as title, version, contact or licensing information.
+     *
+     * @return
+     */
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "TITLE",
+                "DESCIPRION",
+                "VERSION",
+                "TERMS OF SERVICE URL",
+                new Contact("NAME","URL","EMAIL"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
     }
 }
