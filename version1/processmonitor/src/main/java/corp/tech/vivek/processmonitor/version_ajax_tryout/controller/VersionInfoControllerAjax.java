705dea821e555742b6cbc5c4161161227b2125fd
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-12-04 15:29:08 -0500

    Home Page of the application layout done & version block shown using ajax call (#116)

---
+++ version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/version_ajax_tryout/controller/VersionInfoControllerAjax.java
@@ -0,0 +1,62 @@
+package corp.tech.vivek.processmonitor.version_ajax_tryout.controller;
+
+import corp.tech.vivek.processmonitor.version_ajax_tryout.model.VersionInfoRestResponseAjax;
+import corp.tech.vivek.processmonitor.version_info.version_endpoint.VersionInfoModel;
+import corp.tech.vivek.processmonitor.version_info.version_endpoint.VersionInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vivek Vellaiyappan | vivekvellaiyappans@gmail.com
+ */
+@RestController
+public class VersionInfoControllerAjax {
+
+    @Autowired
+    private VersionInfoService versionInfoService;
+
+    @GetMapping("/api/ajax/versions")
+    public VersionInfoRestResponseAjax allEmployees() {
+
+        // instantiating the response object
+        VersionInfoRestResponseAjax response = new VersionInfoRestResponseAjax();
+
+        // retrieve list of versions
+        List<VersionInfoModel> allVersions = versionInfoService.getVersions();
+        VersionInfoModel currentVersionInfoOfTheSystem = allVersions.get(0);
+
+        // make sure we've got versions
+        if (currentVersionInfoOfTheSystem != null) {
+            response.setResponseStatus(VersionInfoRestResponseAjax.OK);
+
+            // put the list in the response object
+            response.setResponse(currentVersionInfoOfTheSystem);
+        } else {
+            response.setResponseStatus(VersionInfoRestResponseAjax.NOT_FOUND);
+            response.setResponse(new ArrayList<VersionInfoModel>());
+        }
+
+        return response;
+    }
+
+    @GetMapping("/api/ajax/versions/consume-response")
+    public void getConsumedVersions() {
+        System.out.println("ajax consume-response done");
+        VersionInfoService versionInfoService = new VersionInfoService();
+        versionInfoService.checkConsumedResponse();
+    }
+
+    @PostMapping("/api/ajax/versions")
+    public void addVersion(@RequestBody VersionInfoModel versions) {
+        System.out.println("ajax post action done... " + versions.getUsername());
+        versionInfoService.addVersions(versions);
+    }
+
+//    @GetMapping("/api/version/{id}")
+//    public Object getVersion(@PathVariable String id) {
+//        return versionInfoService.getVersion(id);
+//    }
+}
