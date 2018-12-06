9a00f2e2c04c764fd2e3283558b077a1549ea9f8
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-12-05 22:23:11 -0500

    awwwwwwwwwwwwwwwwwwwwwwww:) :) :) I am so happygit add .git add .git add . Alright. getting an update for a single row in real time. this will be the base for extending to more than one row that shows the total CPU usage

--- version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/cpu_ajax/controller/CpuInfoControllerAjax.java
+++ version1/processmonitor/src/main/java/corp/tech/vivek/processmonitor/cpu_ajax/controller/CpuInfoControllerAjax.java
@@ -5,6 +5,7 @@ import corp.tech.vivek.processmonitor.cpu_info.cpu_endpoint.CpuInfoModel;
 import corp.tech.vivek.processmonitor.cpu_info.cpu_endpoint.CpuInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,6 +29,34 @@ public class CpuInfoControllerAjax {
         // retrieve a total cpu usage value
         try {
             cpuInfoModel = cpuInfoService.getCpuTotalUsageByID(2l);
+        } catch (NoSuchElementException noSuchEle) {
+            cpuInfoModel = null;
+        }
+
+        // make sure we've got versions
+        if (cpuInfoModel != null) {
+            response.setResponseStatus(CpuInfoRestResponseAjax.OK);
+
+            // put the list in the response object
+            response.setResponse(cpuInfoModel);
+        } else {
+            response.setResponseStatus(CpuInfoRestResponseAjax.NOT_FOUND);
+            response.setResponse(new ArrayList<CpuInfoModel>());
+        }
+
+        return response;
+    }
+
+    @GetMapping("/api/ajax/cpus/{id}")
+    public CpuInfoRestResponseAjax allCpus(@PathVariable Long id) {
+
+        // instantiating the response object
+        CpuInfoRestResponseAjax response = new CpuInfoRestResponseAjax();
+        CpuInfoModel cpuInfoModel;
+
+        // retrieve a total cpu usage value
+        try {
+            cpuInfoModel = cpuInfoService.getCpuTotalUsageByID(id);
         } catch (NoSuchElementException noSuchEle) {
             cpuInfoModel = null;
         }
